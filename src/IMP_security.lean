aefc262f7e392a9d3c5511c63a7bb683720dceea
andreadlm <delmastro2000@gmail.com>
2022-04-14 14:38:19 +0200

    Versione ridotta

--- src/IMP_security.lean
+++ src/IMP_security.lean
@@ -23,30 +23,6 @@ def state_update (s : pstate) (x : vname) (v : val) : pstate :=
 notation s `[` x ` ↦ ` v `]`:100 := state_update s x v
 notation   `[` x ` ↦ ` v `]` := (λ _, 0 ) [x ↦ v]
 
-/--
-Lemma tecnico utile all'utilizzo del tattico `simp` per l'applicazione degli stati
--/
-@[simp] def apply_state_update_pos {x y : vname} {s : pstate} {v : val} :
-  (y = x) → s[x ↦ v] y = v := 
-begin
-  intro,
-  dsimp[state_update],
-  apply if_pos,
-  assumption
-end
-
-/--
-Lemma tecnico utile all'utilizzo del tattico `simp` per l'applicazione degli stati
--/
-@[simp] def apply_state_update_neg {x y : vname} {s : pstate} {v : val} :
-  ¬(y = x) → s[x ↦ v] y = (s y) :=
-begin
-  intro,
-  dsimp[state_update],
-  apply if_neg,
-  assumption
-end
-
 inductive bexp : Type
 | Bc   : bool → bexp
 | Not  : bexp → bexp
@@ -79,104 +55,6 @@ def sec₆ : bexp → lv
 
 notation s ` = ` t ` ⦅<= ` l `⦆` := ∀ (z : vname), (sec z <= l) → s z = t z
 notation s ` = ` t ` ⦅< ` l `⦆` := ∀ (z : vname), (sec z < l) → s z = t z
-
-namespace state_eq_below_lv
-
-lemma refl {s : pstate} {l : lv} : s = s ⦅< l⦆ :=
-begin
-  intros,
-  reflexivity
-end
-
-@[trans] lemma trans {s₁ s₂ s₃ : pstate} {l : lv} : 
-  s₁ = s₂ ⦅< l⦆ →  s₂ = s₃ ⦅< l⦆ → s₁ = s₃ ⦅< l⦆ :=
-begin
-  intros _ _ x _,
-
-  have : s₁ x = s₂ x, from ‹s₁ = s₂ ⦅< l⦆› x ‹sec x < l›,
-  have : s₂ x = s₃ x, from ‹s₂ = s₃ ⦅< l⦆› x ‹sec x < l›,
-
-  show s₁ x = s₃ x, from trans ‹s₁ x = s₂ x› ‹s₂ x = s₃ x›
-end
-
-end state_eq_below_lv
-
-open state_eq_below_lv
-
-lemma noninterference_aexp {s t : pstate} {l : lv} {a : aexp} :
-  s = t ⦅<= l⦆ → (secₐ a <= l) → (aval a s = aval a t) :=
-begin
-  intros,
-  induction' a,
-    case N : {
-      trivial
-    },
-    case V : x {
-      dsimp[secₐ] at *,
-
-      show aval (V x) s = aval (V x) t, by
-        simp_rw[aval, ‹s = t ⦅<= l⦆› x ‹sec x ≤ l›]
-    },
-    case Plus : a₁ a₂ ih₁ ih₂ {
-      dsimp[secₐ] at *,
-
-      have : (secₐ a₁ <= l) ∧ (secₐ a₂ <= l), by 
-        simpa using ‹max (secₐ a₁) (secₐ a₂) ≤ l›,
-      cases' ‹(secₐ a₁ <= l) ∧ (secₐ a₂ <= l)›,
-
-      have : aval a₁ s = aval a₁ t, from ih₁ ‹s = t ⦅<= l⦆› ‹(secₐ a₁ <= l)›,
-      have : aval a₂ s = aval a₂ t, from ih₂ ‹s = t ⦅<= l⦆› ‹(secₐ a₂ <= l)›,
-
-      show aval (Plus a₁ a₂) s = aval (Plus a₁ a₂) t, by
-        simp_rw[aval, ‹aval a₁ s = aval a₁ t›, ‹aval a₂ s = aval a₂ t›]
-    }
-end
-
-lemma noninterference_bexp {s t : pstate} {l : lv} {b : bexp} :
-  s = t ⦅<= l⦆ → (sec₆ b <= l) → (bval b s = bval b t) :=
-begin
-  intros,
-  induction' b,
-    case Bc : {
-      trivial
-    },
-    case Not : {
-      dsimp[sec₆] at *,
-
-      have : bval b s = bval b t, from ih ‹s = t ⦅<= l⦆› ‹sec₆ b ≤ l›,
-
-      show bval (Not b) s = bval (Not b) t, by 
-        simp_rw[bval, ‹bval b s = bval b t›]
-    },
-    case And : b₁ b₂ ih₁ ih₂ {
-      dsimp[sec₆] at *,
-
-      have : (sec₆ b₁ <= l) ∧ (sec₆ b₂ <= l), by
-        simpa using ‹max (sec₆ b₁) (sec₆ b₂) <= l›,
-      cases' ‹(sec₆ b₁ <= l) ∧ (sec₆ b₂ <= l)›,
-
-      have : bval b₁ s = bval b₁ t, from ih₁ ‹s = t ⦅<= l⦆› ‹sec₆ b₁ ≤ l›,
-      have : bval b₂ s = bval b₂ t, from ih₂ ‹s = t ⦅<= l⦆› ‹sec₆ b₂ ≤ l›,
-
-      show bval (And b₁ b₂) s = bval (And b₁ b₂) t, by
-        simp_rw[bval, ‹bval b₁ s = bval b₁ t›, ‹bval b₂ s = bval b₂ t›]
-    },
-    case Less : a₁ a₂ {
-      dsimp[sec₆] at *,
-
-      have : (secₐ a₁ <= l) ∧ (secₐ a₂ <= l), by
-        simpa using ‹max (secₐ a₁) (secₐ a₂) <= l›,
-      cases' ‹(secₐ a₁ <= l) ∧ (secₐ a₂ <= l)›,
-
-      have : aval a₁ s = aval a₁ t, from 
-        noninterference_aexp ‹s = t ⦅<= l⦆› ‹(secₐ a₁ <= l)›,
-      have : aval a₂ s = aval a₂ t, from 
-        noninterference_aexp ‹s = t ⦅<= l⦆› ‹(secₐ a₂ <= l)›,
-
-      show bval (Less a₁ a₂) s = bval (Less a₁ a₂) t, by
-        simp_rw[bval, ‹aval a₁ s = aval a₁ t›, ‹aval a₂ s = aval a₂ t›]
-    }
-end
 
 inductive com : Type
 | SKIP   : com
@@ -257,185 +135,22 @@ infix ` ⊢ₛ `:50 := sec_type
 
 open sec_type
 
-lemma anti_monotonicity {l l' : lv} {c : com} :
-  (l ⊢ₛ c) → (l' <= l) → (l' ⊢ₛ c) :=
-begin
-  intros,
-  induction' ‹l ⊢ₛ c›,
-    case Skip : {
-      show l' ⊢ₛ SKIP, from Skip
-    },
-    case Assign : {
-      have : l' <= sec x, from le_trans ‹l' <= l› ‹l <= sec x›,
-
-      show l' ⊢ₛ x ::= a, from Assign ‹secₐ a ≤ sec x› ‹l' <= sec x›
-    },
-    case Seq : l c₁ c₂ _ _ ih₁ ih₂ {
-      have : l' ⊢ₛ c₁, from ih₁ ‹l' <= l›,
-      have : l' ⊢ₛ c₂, from ih₂ ‹l' <= l›,
-
-      show l' ⊢ₛ c₁ ;; c₂, from Seq ‹l' ⊢ₛ c₁› ‹ l' ⊢ₛ c₂›
-    },
-    case If : _ _ c₁ c₂ _ _ ih₁ ih₂ {
-      have : max (sec₆ b) l' <= max (sec₆ b) l, from max_le_max (le_refl _) ‹l' ≤ l›,
-      
-      have : max (sec₆ b) l' ⊢ₛ c₁, from ih₁ ‹max (sec₆ b) l' <= max (sec₆ b) l›,
-      have : max (sec₆ b) l' ⊢ₛ c₂, from ih₂ ‹max (sec₆ b) l' <= max (sec₆ b) l›,
-
-      show l' ⊢ₛ IF b THEN c₁ ELSE c₂, from 
-        If ‹max (sec₆ b) l' ⊢ₛ c₁› ‹max (sec₆ b) l' ⊢ₛ c₂›
-    },
-    case While : {
-      have : max (sec₆ b) l' <= max (sec₆ b) l, from max_le_max (le_refl _) ‹l' ≤ l›,
-
-      have : max (sec₆ b) l' ⊢ₛ c, from ih ‹max (sec₆ b) l' <= max (sec₆ b) l›,
-
-      show l' ⊢ₛ WHILE b DO c, from While ‹max (sec₆ b) l' ⊢ₛ c›
-    }
-end
-
-lemma confinement {c : com} {s t : pstate} {l : lv} :
-  (c, s) ⟹ t → (l ⊢ₛ c) →  s = t ⦅< l⦆ :=
-begin
-  intros _ _,
-  induction' ‹(c, s) ⟹ t›,
-    case Skip : {
-      show t = t ⦅< l⦆, from refl
-    },
-    case Assign : { -- TODO: migliorabile?
-      intros,
-
-      cases' ‹l ⊢ₛ x ::= a›,
-    
-      have : (z = x) ∨ ¬(z = x), from em (z = x),
-      cases' ‹(z = x) ∨ ¬(z = x)›,
-        case or.inl : {
-          rw ‹z = x› at *,
-          have : ¬(sec x < l), from not_lt_of_le ‹l ≤ sec x›,
-          contradiction
-        },
-        case or.inr : {
-          show t z = t[x ↦ aval a t] z, by simp[‹¬(z = x)›]
-        }
-    },
-    case Seq :  _ _ s s' t _ _ ih₁ ih₂{
-      cases' ‹l ⊢ₛ c₁ ;; c₂›,
-      
-      have : s = s' ⦅< l⦆, from ih₁ ‹l ⊢ₛ c₁›,
-      have : s' = t ⦅< l⦆, from ih₂ ‹l ⊢ₛ c₂›,
-
-      show s = t ⦅< l⦆, from trans ‹s = s' ⦅< l⦆› ‹s' = t ⦅< l⦆›
-    },
-    case IfTrue : {
-      cases' ‹l ⊢ₛ IF b THEN c₁ ELSE c₂›,
-
-      have : l <= max (sec₆ b) l, from le_max_right (sec₆ b) l,
-      have : l ⊢ₛ c₁, from anti_monotonicity ‹max (sec₆ b) l ⊢ₛ c₁› ‹l <= max (sec₆ b) l›,
-
-      show s = t ⦅< l⦆, from ih ‹l ⊢ₛ c₁›
-    },
-    case IfFalse : {
-      cases' ‹l ⊢ₛ IF b THEN c₁ ELSE c₂›,
-
-      have : l <= max (sec₆ b) l, from le_max_right (sec₆ b) l,
-      have : l ⊢ₛ c₂, from anti_monotonicity ‹max (sec₆ b) l ⊢ₛ c₂› ‹l <= max (sec₆ b) l›,
-
-      show s = t ⦅< l⦆, from ih ‹l ⊢ₛ c₂›
-    },
-    case WhileTrue : _ _ s s' t _ _ _ ih₁ ih₂ {
-      cases' ‹l ⊢ₛ WHILE b DO c›,
-
-      have : l <= max (sec₆ b) l, from le_max_right (sec₆ b) l,
-      have : l ⊢ₛ c, from anti_monotonicity ‹max (sec₆ b) l ⊢ₛ c› ‹l <= max (sec₆ b) l›,
-
-      have : s = s' ⦅< l⦆, from ih₁ ‹l ⊢ₛ c›,
-      have : s' = t ⦅< l⦆, from ih₂ (While ‹max (sec₆ b) l ⊢ₛ c›),
-
-      show s = t ⦅< l⦆, from trans ‹s = s' ⦅< l⦆› ‹s'= t ⦅< l⦆›
-    },
-    case WhileFalse : {
-      show t = t ⦅< l⦆, from refl
-    }
-end
-
 theorem noninterference {c : com} {s s' t t' : pstate} {l : lv}:
   (c, s) ⟹ s' → (c, t) ⟹ t' → (0 ⊢ₛ c) → s = t ⦅<= l⦆ → s' = t' ⦅<= l⦆ :=
 begin
   intros _ _ _ _,
   induction' ‹(c, s) ⟹ s'›,
-    case Skip : { 
-      cases' ‹(SKIP, t) ⟹ t'›,
-
-      show s' = t' ⦅<= l⦆, by assumption
-    },
-    case Assign : s _ _ { -- TODO: migliorabile?
-      intros,
-      cases' ‹(x ::= a, t) ⟹ t'› with _ t _,
-      cases' ‹0 ⊢ₛ x ::= a›,
-
-      have : (sec x <= l) ∨ (sec x > l), from le_or_lt (sec x) l,
-      have : (z = x) ∨ ¬(z = x), from em (z = x),
-
-      cases' ‹(z = x) ∨ ¬(z = x)›,
-        case inl : { 
-          simp[‹z = x›] at *,
-
-          cases' ‹(sec x <= l) ∨ (sec x > l)›,
-            case inl : {
-              have : secₐ a <= l, from trans ‹secₐ a ≤ sec x› ‹sec x ≤ l›,
-              
-              show aval a s = aval a t, from 
-                noninterference_aexp ‹s = t ⦅<= l⦆› ‹secₐ a <= l›
-            },
-            case inr : {
-              have : ¬(sec x <= l), from not_le_of_gt ‹sec x > l›,
-              contradiction
-            }
-        },
-        case inr : {
-          simp[‹¬(z = x)›],
-
-          show s z = t z, from ‹s = t ⦅<= l⦆› z ‹sec z ≤ l› 
-        }
-    },
-    case Seq : _ _ s s₁ s' _ _ ih₁ ih₂ {
+    case Skip : { sorry },
+    case Assign : { sorry },
+    case Seq : _ _ s s₁ s' _ _ ih₁ ih₂ { -- PROBLEMATIC CASE
       cases' ‹(c₁ ;; c₂, t) ⟹ t'› with _ _ _ _ _ _ t t₁,
       cases' ‹0 ⊢ₛ c₁ ;; c₂›,
 
       have : s₁ = t₁ ⦅<= l⦆, from ih₁ ‹(c₁, t) ⟹ t₁› ‹0 ⊢ₛ c₁› ‹s = t ⦅<= l⦆›,
       
-      show s' = t' ⦅<= l⦆, from sorry 
+      show s' = t' ⦅<= l⦆, from sorry -- Here I need a more general version of ih₂
     },
-    case IfTrue : {
-      cases' ‹0 ⊢ₛ IF b THEN c₁ ELSE c₂›,
-      cases ‹(IF b THEN c₁ ELSE c₂, t) ⟹ t'›,
-        case IfTrue : {
-          have : sec₆ b ⊢ₛ c₁, from sorry,
-          have : sec₆ b ⊢ₛ c₂, from sorry,
-
-          have : (sec₆ b <= l) ∨ (sec₆ b > l), from le_or_lt (sec₆ b) l,
-          cases' ‹(sec₆ b <= l) ∨ (sec₆ b > l)›,
-            case inl : {
-              have : bval b s = bval b t, from 
-                noninterference_bexp ‹s = t ⦅<= l⦆› ‹sec₆ b ≤ l›,
-
-              have : 0 <= sec₆ b, from sorry,
-              have : 0 ⊢ₛ c₁, from anti_monotonicity ‹sec₆ b ⊢ₛ c₁› ‹0 <= sec₆ b›,
-
-              show s' = t' ⦅<= l⦆, from ih ‹(c₁, t) ⟹ t'› ‹0 ⊢ₛ c₁› ‹s = t ⦅<= l⦆›
-          },
-          case inr : { 
-            have : s = s' ⦅< sec₆ b⦆, from confinement ‹(c₁, s) ⟹ s'› ‹sec₆ b ⊢ₛ c₁›,
-            have : t = t' ⦅< sec₆ b⦆, from confinement ‹(c₁, t) ⟹ t'› ‹sec₆ b ⊢ₛ c₁›,
-
-            have : s = s' ⦅<= l⦆, from sorry,
-            have : t = t' ⦅<= l⦆, from sorry,
-
-            show s' = t' ⦅<= l⦆, from sorry
-          }
-        },
-        case IfFalse : { sorry }
-    },
+    case IfTrue : { sorry },
     case IfFalse : { sorry },
     case WhileTrue : { sorry },
     case WhileFalse : { sorry } 
