ab0c4a83bca6483ea749ef6dee388dca1e8bb82e
andreadlm <delmastro2000@gmail.com>
2022-05-17 19:07:11 +0200

    Documentazione

--- src/IMP_op_sem.lean
+++ src/IMP_op_sem.lean
@@ -1,24 +1,23 @@
 /-
 Authore: Andrea Delmastro
 -/
-
 import tactic.induction
 
 /-!
 # Semantica per IMP
 
-Questo file definisce le semantiche big-step e small-step per il linguaggio di programmazione IMP e ne
-dimostra l'equivalenza.
+Questo file definisce le semantiche big-step e small-step per il linguaggio di programmazione IMP e 
+ne dimostra l'equivalenza.
 
-La semantica di un linguaggio di programmazione descrive formalmente il processo compiuto da un calcolatore
-nell'esecuzione di un programma scritto in un dato linguaggio.
+La semantica di un linguaggio di programmazione fornisce una descrizione computazionale delle 
+strutture sintattiche del linguaggio.
 
 ## Note di implementazione
 
-Questo file utilizza per alcune dimostrazioni i tattici `cases'` e `induction'` definiti in `tactic.induction`. 
-Nonostante le medesime dimostrazioni siano ottenibili tramite i tattici `cases` e `induction` standard, le due 
-versioni qui utilizzate meglio si prestano all'utilizzo con predicati induttivi e sono più 'naturali' rispetto alla
-medesima dimostrazione svolta su carta.
+Questo file utilizza i tattici `cases'` e `induction'` definiti in `tactic.induction`. 
+Nonostante le medesime dimostrazioni siano ottenibili tramite i tattici `cases` e `induction` 
+classici, le due versioni qui utilizzate meglio si prestano all'utilizzo con predicati induttivi e 
+sono più 'naturali' rispetto alla medesima dimostrazione svolta su carta.
 
 ## Notazione
 
@@ -34,6 +33,12 @@ medesima dimostrazione svolta su carta.
 * `↝` : riduzione small-step
 * `↝*` : chiusura riflessiva e transitiva di `↝`
 
+## Teoremi principali
+
+* `big_step.deterministic : ∀ {c : com} {s t r : pstate}, (c, s) ⟹ t → (c, s) ⟹ r → t = r`
+* `small_step.deterministic : ∀ {c : com} {s : pstate} {cs₁ cs₂ : conf}, (c, s)↝cs₁ → (c, s)↝cs₂ → cs₁ = cs₂`
+* `big_step_equiv_small_step : ∀ {c : com} {s t : pstate} (c, s) ⟹ t ↔ (c, s)↝*(SKIP, t)`
+
 ## Riferimenti
 
 * [Nipkow, Klein, Concrete Semantics with Isabelle/HOL][Nipkow]
@@ -45,6 +50,7 @@ abbreviation pstate := vname → val
 
 /-!
 ### Espressioni aritmetiche
+
 Sintassi e valutazione
 -/
 
@@ -55,16 +61,28 @@ inductive aexp : Type
 
 open aexp
 
+/--
+Funzione di valutazione di un'espressione aritmetica: associa ad ogni espressione il proprio valore.
+Ogni espressione ha un valore ben definito.
+-/
 def aval : aexp → pstate → val
 | (N n)        _ := n
 | (V x)        s := s x
 | (Plus a₁ a₂) s := (aval a₁ s) + (aval a₂ s)
 
+/--
+Aggiornamento dello stato: associa ad una variabile entro uno stato un nuovo valore.
+-/
 def state_update (s : pstate) (x : vname) (v : val) : pstate :=
-  λ y, if (y = x) then v else s y
+  λ (y : vname), if (y = x) then v else s y
+
+/--
+Stato vuoto, associa ad ogni variabile il valore 0.
+-/
+def emp : pstate := (λ _, 0)
 
 notation s `[` x ` ↦ ` v `]`:100 := state_update s x v
-notation   `[` x ` ↦ ` v `]` := (λ _, 0 ) [x ↦ v]
+notation   `[` x ` ↦ ` v `]`     := emp [x ↦ v]
 
 /-!
 ### Espressioni booleane
@@ -79,6 +97,10 @@ inductive bexp : Type
 
 open bexp
 
+/--
+Funzione di valutazione di un'espressione booleana: associa ad ogni espressione il proprio valore.
+Ogni espressione ha un valore ben definito.
+-/
 def bval : bexp → pstate → bool
 | (Bc v)       _ := v
 | (Not b)      s := ¬(bval b s)
@@ -108,10 +130,10 @@ open com
 /-!
 ### Semantica operazionale big-step di IMP
 
-La semantica big-step ([Nipkow] Fig. 7.1) è una relazione a tre posti il cui significato è `(c, s)⇒ t` se 
-l'esecuzione del comando c nello stato iniziale s termina con lo stato  t. Gli stati intermedi nell'esecuzione
-del programma non sono visibili nella relazione: la relazione in sé considera come se il programma venisse
-eseguito in un unico grande step.
+La semantica big-step ([Nipkow] Fig. 7.1) è una relazione a tre posti il cui significato è 
+`(c, s)⇒ t` se l'esecuzione del comando c nello stato iniziale s termina con lo stato  t. Gli stati 
+intermedi nell'esecuzione del programma non sono visibili nella relazione: la relazione in sé 
+considera il programma come se venisse eseguito in un unico grande step.
 -/
 
 inductive big_step : conf → pstate → Prop
@@ -154,24 +176,48 @@ namespace big_step
 
 namespace rule_inversion
 
+/-!
+### Inversione delle regole
+
+Le regole di inversione per la semantica big-step permettono di inferire informazioni a partire
+da termini di tipo `(c, s)⇒ t`. Dipendono dal fatto che un predicato induttivo è vero solo se le
+regole di inferenza lo permettono, ovvero esiste una derivazione per il predicato. 
+-/
+
+/--
+Regola di inversione per `Skip`: da `(SKIP, s) ⟹ t` è possibile inferire `s = t`.
+-/
 lemma skip {s t : pstate} (h : (SKIP, s) ⟹ t) : 
-  t = s :=
-begin cases ‹(SKIP, s) ⟹ t›, reflexivity end
+  s = t :=
+begin cases' ‹(SKIP, s) ⟹ t›, reflexivity end
 
+/--
+Regola di inversione per `Assign`: da `(x ::= a, s) ⟹ t` è possibile inferire 
+`s[x ↦ aval a s] = t`.
+-/
 lemma assign {x : vname} {a : aexp} {s t : pstate} (h : (x ::= a, s) ⟹ t) : 
-  t = (s[x ↦ aval a s]) :=
-begin cases ‹(x ::= a, s) ⟹ t›, reflexivity end 
+  s[x ↦ aval a s] = t :=
+begin cases' ‹(x ::= a, s) ⟹ t›, reflexivity end 
 
+/--
+Regola di inversione per `Seq`: da `(c₁ ;; c₂, s₁) ⟹ s₃` è possibile inferire l'esistenza di uno
+stato intermedio `s₂` tale che `(c₁, s₁) ⟹ s₂` e `(c₂, s₂) ⟹ s₃`.
+-/
 lemma seq {c₁ c₂ : com} {s₁ s₃ : pstate} (h : (c₁ ;; c₂, s₁) ⟹ s₃) : 
   ∃ (s₂ : pstate), (c₁, s₁) ⟹ s₂ ∧ (c₂, s₂) ⟹ s₃ :=
 begin
-  cases ‹(c₁ ;; c₂, s₁) ⟹ s₃›,
+  cases' ‹(c₁ ;; c₂, s₁) ⟹ s₃› with _ _ _ _ _ _ _ s₂,
   apply exists.intro,
   split,
-    exact ‹(c₁, s₁) ⟹ h_s₂›,
-    exact ‹(c₂, h_s₂) ⟹ s₃›
+    exact ‹(c₁, s₁) ⟹ s₂›,
+    exact ‹(c₂, s₂) ⟹ s₃›
 end
 
+/--
+Regola di inversione per `IfTrue` e `IfFalse`: da `(IF b THEN c₁ ELSE c₂, s) ⟹ t` è possibile
+inferire `(bval b s ∧ (c₁, s) ⟹ t)`, corrispondente a una derivazione che termina in `IfTrue` 
+oppure `(¬bval b s ∧ (c₂, s) ⟹ t)`, corrispondente a una derivazione che termina in `IfFalse`
+-/
 lemma ifte {b : bexp} {c₁ c₂ : com} {s t : pstate} (h : (IF b THEN c₁ ELSE c₂, s) ⟹ t) :
   (bval b s ∧ (c₁, s) ⟹ t) ∨ (¬bval b s ∧ (c₂, s) ⟹ t) :=
 begin
@@ -180,13 +226,19 @@ begin
     case IfFalse : { exact or.inr ⟨‹¬↥(bval b s)›, ‹(c₂, s) ⟹ t›⟩ }
 end
 
+/--
+Regola di inversione per `WhileTrue` e `WhileFalse`: da `(WHILE b DO c, s) ⟹ t` è possibile inferire 
+`(¬bval b s ∧ t = s)`, corrispondente a una derivazione che termina in `WhileFalse` oppure 
+`(bval b s ∧ ∃ (s₁ : pstate), (c, s) ⟹ s₁ ∧ (WHILE b DO c, s₁) ⟹ t)`, corrispondente a una
+derivazione che termina in `WhileTrue`.
+-/
 lemma while {b : bexp} {c : com} {s t : pstate} (h : (WHILE b DO c, s) ⟹ t) :
   (¬bval b s ∧ t = s) ∨ (bval b s ∧ ∃ (s₁ : pstate), (c, s) ⟹ s₁ ∧ (WHILE b DO c, s₁) ⟹ t) :=
 begin
   cases ‹(WHILE b DO c, s) ⟹ t›,
-    case WhileTrue : { 
+    case WhileTrue : _ _ _ s₂ _ _ _ _ { 
       have : ∃ (s₁ : pstate), (c, s) ⟹ s₁ ∧ (WHILE b DO c, s₁) ⟹ t := 
-        ⟨h_s₂, ⟨‹(c, s) ⟹ h_s₂›, ‹(WHILE b DO c, h_s₂) ⟹ t›⟩⟩,
+        ⟨s₂, ⟨‹(c, s) ⟹ s₂›, ‹(WHILE b DO c, s₂) ⟹ t›⟩⟩,
       exact or.inr ⟨‹↥(bval b s)›, ‹∃ (s₁ : pstate), (c, s) ⟹ s₁ ∧ (WHILE b DO c, s₁) ⟹ t›⟩ 
     },
     case WhileFalse : { exact or.inl ⟨‹¬↥(bval b s)›, rfl⟩ }
@@ -194,6 +246,9 @@ end
 
 end rule_inversion
 
+/--
+Associatività della composizione sequenziale.
+-/
 lemma seq_assoc {c₁ c₂ c₃ : com} {s t : pstate} :
   (c₁ ;; c₂ ;; c₃, s) ⟹ t ↔ (c₁ ;; (c₂ ;; c₃), s) ⟹ t :=
 begin
@@ -231,13 +286,18 @@ namespace com_equivalence
 /-!
 ### Equivalenza di comandi
 
-Due comandi `c₁` e `c₂` sono definiti equivalenti se a partire dallo stesso s stato terminano nel medesimo 
-stato `t`. 
+Due comandi `c₁` e `c₂` sono definiti equivalenti se a partire dallo stesso s stato terminano nel 
+medesimo stato `t`. 
 -/
 
-notation c₁ `∼` c₂ := ∀{s t : pstate}, (c₁, s) ⟹ t = (c₂, s) ⟹ t
+notation c₁ `∼` c₂ := ∀ {s t : pstate}, (c₁, s) ⟹ t = (c₂, s) ⟹ t
 
-lemma eq_while_ifwhile {b : bexp} {c : com} : WHILE b DO c ∼ IF b THEN (c ;; WHILE b DO c) ELSE SKIP :=
+/--
+Il comando `WHILE d DO c` è equivalente al suo unfolding in 
+`IF b THEN (c ;; WHILE b DO c) ELSE SKIP`.
+-/
+lemma eq_while_ifwhile {b : bexp} {c : com} : 
+  WHILE b DO c ∼ IF b THEN (c ;; WHILE b DO c) ELSE SKIP :=
 begin
   intros,
   apply iff.to_eq,
@@ -285,7 +345,7 @@ begin
       cases' ‹(SKIP, s) ⟹ r›, 
       trivial 
     },
-    case Assign : s x a { 
+    case Assign : s x a {
       cases' ‹(x ::= a, s) ⟹ r›, 
       trivial 
     },
@@ -312,7 +372,7 @@ begin
           rw ←‹s₁ = s₂› at *,
           show t = r, from ih₂ ‹(WHILE b DO c, s₁) ⟹ r›
         },
-        case WhileFalse : { trivial  }
+        case WhileFalse : { trivial }
     },
     case WhileFalse : _ _ s _ { 
       cases' ‹(WHILE b DO c, s) ⟹ r›, 
@@ -326,9 +386,9 @@ end big_step
 /-!
 ### Semantica operazionale small-step di IMP
 
-La riduzione small-step ([Nipkow] Fig. 7.5)  `(c₁ , s₁)→(c₂ , s₂)` è una relazione tra due coppie comando/stato
-che rappresenta l'esecuzione di un singolo passo di calcolo di `c₁` nello stato `s₁` che produce lo stato `s₂`, 
-mentre `c₂` é la continuazione (ciò che resta da eseguire) di `c₁`. 
+La riduzione small-step ([Nipkow] Fig. 7.5)  `(c₁ , s₁)→(c₂ , s₂)` è una relazione tra due coppie 
+comando/stato che rappresenta l'esecuzione di un singolo passo di calcolo di `c₁` nello stato `s₁` 
+che produce lo stato `s₂`, mentre `c₂` é la continuazione (ciò che resta da eseguire) di `c₁`. 
 -/
 
 inductive small_step : conf → conf → Prop
@@ -336,7 +396,7 @@ inductive small_step : conf → conf → Prop
   small_step (x ::= a, s) (SKIP, s[x ↦ aval a s])
 
 | Seq1 {c₂ : com} {s : pstate} :
-  small_step (SKIP;;c₂, s) (c₂, s)
+  small_step (SKIP ;; c₂, s) (c₂, s)
 
 | Seq2 {c₁ c₁' c₂ : com} {s s' : pstate} :
   small_step (c₁, s) (c₁', s') →
@@ -432,49 +492,29 @@ begin
      small_step_star_small_step_trans ‹(c, s)↝*(c₁, s₁)› ‹(c₁, s₁)↝(c₂, s₂)›
 end
 
-section small_step_calc_ex
-
-/-!
-### Esempio di utilizzo dell'ambiente `calc` con ↝ e ↝*
--/
-
-open bexp
-
-variable s : pstate
-variable p : ↥(bval (Bc tt) s) 
-
-example : (IF (Bc tt) THEN SKIP ;; SKIP ELSE SKIP, s)↝*(SKIP, s) :=
-  calc
-    (IF (Bc tt) THEN SKIP ;; SKIP ELSE SKIP, s)↝ (SKIP ;; SKIP, s) : small_step.IfTrue p
-    ...                                        ↝ (SKIP, s)         : small_step.Seq1
-    ...                                        ↝*(SKIP, s)         : small_step_star.refl 
-
-end small_step_calc_ex
-
 end trans
 
 /--
 Determinismo della semantica small-step. 
 Se la configurazione `(c, s)` si riduce di un passo, allora esiste un'unica configurazione `cs` tale 
-che `(c, s) ↝ cs`.
+che `(c, s)↝cs`.
 -/
-theorem deterministic {c : com} {s : pstate} {cs₁ cs₂ : conf}: 
-  (c, s)↝cs₁ ∧ (c, s)↝cs₂ → cs₁ = cs₂ :=
+theorem deterministic {c : com} {s : pstate} {cs₁ cs₂ : conf} : 
+  (c, s)↝cs₁ → (c, s)↝cs₂ → cs₁ = cs₂ :=
 begin
   intros,
-  cases ‹(c, s)↝cs₁ ∧ (c, s)↝cs₂›,
   induction' ‹(c, s)↝cs₁›,
     case Assign : s x a {
-      cases ‹(x ::= a, s)↝cs₂›,
+      cases' ‹(x ::= a, s)↝cs₂›,
       trivial
     },
     case Seq1 : c₂ s {
-      cases ‹(SKIP ;; c₂, s)↝cs₂›,
+      cases' ‹(SKIP ;; c₂, s)↝cs₂›,
         case Seq1 : { trivial },
-        case Seq2 : c₁' _ _ s' { cases ‹(SKIP, s)↝(c₁', s')› }
+        case Seq2 : c₁' _ _ s' { cases' ‹(SKIP, s)↝(c₁', s')› }
     },
     case Seq2 : c₁ c₁' c₂ s s' _ ih {
-      cases ‹(c₁ ;; c₂, s)↝cs₂›,
+      cases' ‹(c₁ ;; c₂, s)↝cs₂›,
         case Seq1 : { cases ‹(SKIP, s)↝(c₁', s')› },
         case Seq2 : _ c₁'' _ _ s'' _ {
           have : (c₁', s') = (c₁'', s'') := ih ‹(c₁, s)↝(c₁'', s'')›,
@@ -484,17 +524,17 @@ begin
         }
     },
     case IfTrue : b s c' c'' {
-      cases ‹(IF b THEN c' ELSE c'', s)↝cs₂›,
+      cases' ‹(IF b THEN c' ELSE c'', s)↝cs₂›,
         case IfTrue : { trivial },
         case IfFalse : { trivial }
     },
     case IfFalse : b s c' c'' {
-      cases ‹(IF b THEN c' ELSE c'', s)↝cs₂›,
+      cases' ‹(IF b THEN c' ELSE c'', s)↝cs₂›,
         case IfTrue : { trivial },
         case IfFalse : { trivial }
     },
     case While : b c s {
-      cases ‹(WHILE b DO c, s)↝cs₂›,
+      cases' ‹(WHILE b DO c, s)↝cs₂›,
       trivial
     }                                           
 end
@@ -511,27 +551,25 @@ begin
   intro,
   induction' ‹(c₁, s₁)↝*(c, s₂)›,
     case refl : { exact refl },
-    case step : c₁ c₃ c s₁ s₃ s₂ {
-      have : (c₁ ;; c₂, s₁)↝(c₃ ;; c₂, s₃) := Seq2 ‹(c₁, s₁)↝(c₃, s₃)›,
-      have : (c₃ ;; c₂, s₃)↝*(c ;; c₂, s₂) := ih,
-      exact step ‹(c₁ ;; c₂, s₁)↝(c₃ ;; c₂, s₃)› ‹(c₃ ;; c₂, s₃)↝*(c ;; c₂, s₂)›
+    case step : c₁ c₃ c s₁ s₃ s₂ { 
+      show (c₁ ;; c₂, s₁)↝*(c ;; c₂, s₂), from
+        calc
+          (c₁ ;; c₂, s₁)↝ (c₃ ;; c₂, s₃) : Seq2 ‹(c₁, s₁)↝(c₃, s₃)›
+          ...           ↝*(c ;; c₂, s₂)  : ih
     }
 end
 
 /--
 La semantica big-step implica la semantica small-step.
-Se l'esecuzione di `c` in `s` termina in uno stato `t`, allora `(c, s)` si riduce in zero o più passi a 
-`(SKIP, t)`.
+Se l'esecuzione di `c` in `s` termina in uno stato `t`, allora `(c, s)` si riduce in zero o più 
+passi a `(SKIP, t)`.
 -/
 lemma big_step_imp_small_step {c : com} {s t : pstate} : 
   (c, s) ⟹ t → (c, s)↝*(SKIP, t) :=
 begin
   intro,
   induction' ‹(c, s) ⟹ t›,
-    case Skip : {
-      calc
-        (SKIP, t)↝*(SKIP, t) : refl
-    },
+    case Skip : { exact refl },
     case Assign : {
       calc
         (x ::= a, t)↝ (SKIP, t[x ↦ aval a t]) : Assign
@@ -559,7 +597,7 @@ begin
       calc
         (WHILE b DO c, s)↝ (IF b THEN c ;; WHILE b DO c ELSE SKIP, s) : While
         ...              ↝ (c ;; WHILE b DO c, s)                     : IfTrue ‹↥(bval b s)›
-        ...              ↝*(SKIP ;; WHILE b DO c, s₁)                 : seq_star ‹(c, s)↝*(SKIP, s₁)›
+        ...              ↝*(SKIP ;; WHILE b DO c, s₁)                 : seq_star ih₁
         ...              ↝ (WHILE b DO c, s₁)                         : Seq1
         ...              ↝*(SKIP, t)                                  : ih₂
     },
@@ -571,8 +609,8 @@ begin
 end
 
 /--
-Se la configurazione `(c, s)` si riduce in un passo a `(c₁, s₁)` e l'esecuzione di `c₁` in `s₁` termina in `t`, 
-allora l'esecuzione di `c` in `s` termina in `t`.
+Se la configurazione `(c, s)` si riduce in un passo a `(c₁, s₁)` e l'esecuzione di `c₁` in `s₁` 
+termina in `t`, allora l'esecuzione di `c` in `s` termina in `t`.
 -/
 lemma step_case {c c₁ : com} {s s₁ t : pstate} : 
   (c, s)↝(c₁, s₁) → (c₁, s₁) ⟹ t → (c, s) ⟹ t :=
@@ -616,9 +654,10 @@ end
 
 /--
 La semantica small-step implica la semantica big-step.
-Se la configurazione `(c, s)` si riduce in zero o più passi a `(SKIP, t)`, allora l'esecuzione di `c` in `s`
-termina nello stato `t`.
-La dimostrazione per il caso `refl` é banale, mentre nel caso `step` segue immediatamente dal lemma `step_case`.
+Se la configurazione `(c, s)` si riduce in zero o più passi a `(SKIP, t)`, allora l'esecuzione di 
+`c` in `s` termina nello stato `t`.
+La dimostrazione per il caso `refl` é banale, mentre nel caso `step` segue immediatamente dal lemma 
+`step_case`.
 -/
 lemma small_step_imp_big_step {c : com} {s t : pstate} :
   (c, s)↝*(SKIP, t) → (c, s) ⟹ t :=
